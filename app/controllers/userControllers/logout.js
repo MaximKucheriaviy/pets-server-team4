4c63056e0760682900a63cf4f7ea1c758e1cb465
Maxim Kucheriaviy <maxim-k@i.ua>
2023-01-28 00:34:17 +0200

    ready do work

--- app/controllers/userControllers/logout.js
+++ app/controllers/userControllers/logout.js
@@ -1,17 +1,14 @@
 const { User } = require("../../models");
 
-
 const logout = async (req, res, next) => {
-    try{
+  try {
+    await User.findByIdAndUpdate(req._id, { token: null });
+    res.json({
+      message: "The user is logged out",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-        await User.findByIdAndUpdate(req._id, {token: null});
-        res.json({
-            message: "The user is logged out"
-        })
-    }
-    catch(err){
-        next(err);
-    }
-}
-
-module.exports = logout
+module.exports = logout;
