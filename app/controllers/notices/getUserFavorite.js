b492e4f809b608bfee42154f082b65d11e7b4035
he1gi <oleh.mishchenko1@gmail.com>
2023-02-04 17:43:39 +0100

    notices/fixed favorite get method

--- app/controllers/notices/getUserFavorite.js
+++ app/controllers/notices/getUserFavorite.js
@@ -2,8 +2,8 @@ const { Notice } = require("../../models/noticeModel");
 const { User } = require("../../models/userModel");
 
 const getUserFavorite = async (req, res) => {
-  const user = await User.find({ _id: req._id });
-  res.json(user);
+  const result = await User.find({ _id: req._id }).populate("favoriteNotices");
+  res.json(result);
 };
 
 module.exports = getUserFavorite;
