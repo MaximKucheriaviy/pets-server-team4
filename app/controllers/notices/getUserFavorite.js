3679f0b3c635ec47980285a4aebb09250fac4b31
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-02-09 21:07:56 +0200

    favorite

--- app/controllers/notices/getUserFavorite.js
+++ app/controllers/notices/getUserFavorite.js
@@ -1,10 +1,10 @@
 const { User } = require("../../models/userModel");
 
 const getUserFavorite = async (req, res) => {
-  const result = await User.find({ _id: req._id }).populate(
+  const result = await User.findOne({ _id: req._id }).populate(
     "favoriteNotices"
   );
-  res.json(result.reverse());
+  res.json(result);
 };
 
 module.exports = getUserFavorite;
